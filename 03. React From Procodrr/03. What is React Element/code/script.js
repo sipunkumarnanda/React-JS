f536c8d0a9ef274756e8f5c902e22344205af924
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2025-07-17 08:24:22 +0000

    Using React by CDN

--- 03. React From Procodrr/03. What is React Element/code/script.js
+++ 03. React From Procodrr/03. What is React Element/code/script.js
@@ -9,9 +9,35 @@ let heading2 = React.createElement("h2", {
     key : "heading-2"
 }, "Hello World From JS Heading-2")
 
+// let h2 = React.createElement("h2", {
+//     className : "subheading",
+//     id : "subtitle",
+//     key : "heading-3"
+// }, "Hello iam Subheading")
+
+// console.log(h2);
+
+
+
+// ✅ Manually creating a valid React element object (not using JSX or React.createElement)
+// ⚠️ This is for educational purposes; normally, use JSX or React.createElement
+
+let h2 = {
+    $$typeof : Symbol.for("react.element"),
+    type : "h2",
+    key : "heading-3",
+    ref : null,
+    props : {
+        className : "subheading",
+        id : "subtitle",
+        children : "Hello iam Subheading created by OBJECT"
+    }
+}
+
+
 let conatiner = React.createElement("div", {
     className : "conatiner"
-},[heading1, heading2])
+},[heading1, heading2, h2])
 
 
 let root = ReactDOM.createRoot(document.getElementById("root"))
